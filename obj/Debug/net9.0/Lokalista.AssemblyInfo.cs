1fd02f985ff6a0f24a4438982aed062e1084b60a
DuayRacist <duayjerick@gmail.com>
2026-04-29 02:27:28 +0800

    dsfsdfd

--- obj/Debug/net9.0/Lokalista.AssemblyInfo.cs
+++ obj/Debug/net9.0/Lokalista.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lokalista")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a709987e2ee5bd0f7c85aaf31b823981dd8afaec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+558b0be39e2f5e37e3344a4ddd885c55031f6c4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lokalista")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lokalista")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
